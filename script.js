68d168a5f7a38712e926ece2499d5cbf2e8e04b4
Jimmy Jam <jimmymano@mail.com>
2021-09-30 00:58:53 -0500

    bug fix

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('testing');
+const {body} = document;
+
+function changeBackground(number){
+
+        // Reset CSS class on body
+        body.className = '';
+
+    switch(number){
+        case'1':
+        body.classList.add('background-1');
+        break;
+        case'2':
+        body.classList.add('background-2');
+        break;
+        case'3':
+        body.classList.add('background-3');
+        break;
+        default:break;
+    }
+}
